a04e0621290609a6ff490a5556457d94ef1cdc97
Albert Cobos <acobos@ub.edu>
2025-10-23 19:25:11 +0200

    quant paired exe solution

---
+++ 4_4_quant_paired_exe_solution.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(ggformula)
+
+
+# Read data and compute differences
+fileURL <- "https://raw.githubusercontent.com/acobos/Datasets/master/Life_Expectancy_with_continent.csv"
+d <- rio::import(fileURL) %>% 
+  mutate(difference = LE_women - LE_men)
+head(d)
+
+# inspection of distribution of differences
+gf_boxplot(difference ~ "", data=d, xlab="", ylab="difference women - men)")  %>% 
+  gf_summary(fun = mean, geom="point", color="red")  %>% 
+  gf_refine(coord_flip())
+
+# slightly assymetrical, no outliers, n = 191
+# difference mean and median doesn't look very relevant (< 1 y)
+## t-test is OK
+t.test(d$difference)
+t.test(d$LE_women, d$LE_men, paired = TRUE)        # equivalent
+
+# Provide some estimate of the difference
+res <- t.test(d$LE_women, d$LE_men, paired = TRUE) 
+names(res)
+res$estimate
+res$conf.int
