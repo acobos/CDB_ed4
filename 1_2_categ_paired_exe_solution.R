4a8e5ab7c8db3a04b22fb20ab52a933cb9299ff0
Albert Cobos <acobos@ub.edu>
2025-10-30 18:47:32 +0100

    Edits to compute Sens & Espe with mosaic::tally() using pipes

--- 1_2_categ_paired_exe_solution.R
+++ 1_2_categ_paired_exe_solution.R
@@ -2,29 +2,37 @@ library(tidyverse)
 library(mosaic)
 
 # reading the data
-d <- rio::import("data/bta.xls")
+d <- rio::import("bta.xls")
 head(d)
 
 
 # BTA Sens & Spec
 tally(bta ~ cystoscopy, data=d)
-tally(bta ~ cystoscopy, data=d, format="percent")
+tally(bta ~ cystoscopy, data=d, format="percent") %>% round(1)
 
 # Cytology Sens & Spec
 tally(cytology ~ cystoscopy, data=d)
-tally(cytology ~ cystoscopy, data=d, format="percent")
+tally(cytology ~ cystoscopy, data=d, format="percent") %>% round(1)
 
 
 # compare sensitivities
-x <- tally(bta ~ cytology, data=d, subset = cystoscopy == "Positive")
-x
-mcnemar.test(x)
+d %>% 
+  filter(cystoscopy == "Positive") %>%         # keep only these patients!
+  tally(bta ~ cytology)                        # this gives an error...
+
+# because arg data= in mosaic::tally() is not the first arg!
+# can be done with the data=. trick
+d %>% 
+  filter(cystoscopy == "Positive") %>%         # keep only these patients!
+  tally(bta ~ cytology, data=.)                
+
+# convenient altervnative
+tally(bta ~ cytology, data=d, subset = cystoscopy == "Positive") %>% 
+  mcnemar.test(x)
 
 # compare specificities
-x <- tally(bta ~ cytology, data=d, subset = cystoscopy == "Negative")
-x
-mcnemar.test(x)
-
+tally(bta ~ cytology, data=d, subset = cystoscopy == "Negative") %>% 
+  mcnemar.test(x)
 
 # agreement
 x <- tally(bta ~ cytology, data=d)
@@ -35,8 +43,6 @@ epi.kappa(x)$prop.agree
 epi.kappa(x)$kappa
 
 
-
-
 # EXTRA: diagnostic accuracy measures with function ThresholdROC::diagnostic()
 
 ?ThresholdROC::diagnostic
@@ -45,8 +51,11 @@ tally(bta ~ cystoscopy, data=d)                       # not the table we need!
 
 tally(fct_rev(bta) ~ fct_rev(cystoscopy), data=d)     # this table is ok
 
+# accuracy measures for BTA test
+tally(fct_rev(bta) ~ fct_rev(cystoscopy), data=d) %>% 
+  ThresholdROC::diagnostic()
 
-tally(fct_rev(bta) ~ fct_rev(cystoscopy), data=d) %>% ThresholdROC::diagnostic()
-
-tally(fct_rev(cytology) ~ fct_rev(cystoscopy), data=d) %>% ThresholdROC::diagnostic()
+# accuracy measures for cytology
+tally(fct_rev(cytology) ~ fct_rev(cystoscopy), data=d) %>% 
+  ThresholdROC::diagnostic()
 
